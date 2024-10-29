d9fc63a6c57cd734b751179a1a57ca7679ad2539
Abdullah Ali <162645001+MAbdullah005@users.noreply.github.com>
2024-10-28 22:08:07 -0700

    Update car.h

--- car.h
+++ car.h
@@ -1,1 +1,39 @@
+#include <iostream>
+#include <string>
+#include <ctime>
+#include <iomanip>
+#ifndef CAR_H
+#define CAR_H
+
+using namespace std;
+
+class Car {
+private:
+    string model;
+    string make;
+    double price;
+    time_t buyDate;
+    time_t sellDate;
+
+public:
+    Car(const string &m, const string &mk, double p);
+
+    void sell();
+    
+    void setModel(const string &m);
+
+    string getModel() const;
+    
+    void setMake(const string &mk);
+
+    string getMake() const;
+    
+    void setPrice(double p);
+
+    double getPrice() const;
+
+    void display() const;
+};
+
+#endif 
 ﻿
