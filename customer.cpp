1c834f223f56238441adadc56656a6d7d4c8fe87
Abdullah Ali <162645001+MAbdullah005@users.noreply.github.com>
2024-10-28 22:07:23 -0700

    Update customer.cpp

--- customer.cpp
+++ customer.cpp
@@ -1,1 +1,36 @@
+#include "Customer.h"
+
+Customer::Customer(const string &n, const string &cn, const string &pn)
+    : name(n), cnic(cn), phoneNumber(pn) {}
+
+void Customer::setCnic(const string &cn)
+{
+    cnic = cn;
+}
+string Customer::getCnic() const 
+{ 
+    return cnic; 
+}
+
+void Customer::setPhoneNumber(const string &pn)
+{
+    phoneNumber = pn; 
+}
+string Customer::getPhoneNumber() const
+{
+    return phoneNumber;
+}
+
+void Customer::buyCar(const Car &car)
+{
+    cars.push_back(car);
+}
+
+void Customer::displayCars() const 
+{
+    cout << "Cars owned by " << name << " (CNIC: " << cnic << ", Phone: " << phoneNumber << "):\n";
+    for (const auto &car : cars) {
+        car.display();
+    }
+}
 ﻿
