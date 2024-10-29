07a2752523971b8cc822af63a7ae11a944b63e29
Abdullah Ali <162645001+MAbdullah005@users.noreply.github.com>
2024-10-28 22:06:01 -0700

    Add files via upload

---
+++ program.cpp
@@ -0,0 +1,11 @@
+﻿#include "Customer.h"
+
+int main() {
+    Customer customer("John Doe", "12345-6789012-3", "555-1234");
+    Car car1("Model S", "Tesla", 79999);
+    customer.buyCar(car1);
+    car1.sell();
+    
+    customer.displayCars();
+    return 0;
+}
